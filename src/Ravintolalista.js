7cdd8ae44c3f501c673b6af9fedb76da23e90ad7
Topias Mariapori <Mariapori@users.noreply.github.com>
2022-05-28 21:22:11 +0000

    Bootstrappia

--- src/Ravintolalista.js
+++ src/Ravintolalista.js
@@ -1,23 +1,38 @@
 import React, {useState, useEffect} from 'react';
-import './App.css';
+import { Card } from 'react-bootstrap';
 function Ravintolalista() {
   
   const [ravintolat,setRavintolat] = useState([]);
+  const [ravintola,setRavintola] = useState([]);
   useEffect(()=>{
     fetch("https://digiruokalista.com/api/v1/HaeYritykset")  
     .then(response => response.json())
     .then(data => setRavintolat(data))
     .catch(() => alert("Ruokalistoja ei voitu hakea."));
+    document.getElementById("ravintola").style = "display: none;";
   },[]);
+
+  function nakkivene(rafla){
+    document.getElementById("titteli").textContent = rafla.nimi;
+    document.getElementById("ravintolalista").style = "display: none;";
+    setRavintola(rafla.ruokalista.kategoriat);
+    document.getElementById("ravintola").style = "display: block;";
+  }
+  function Palaa(){
+    document.getElementById("titteli").textContent = "Ravintolat";
+    document.getElementById("ravintola").style = "display: none;"
+    document.getElementById("ravintolalista").style = "display: block;";
+  }
   return (
     <div>
-      <header>
-        <h1>Digiruokalista React</h1>
-      </header>
+    <div class="jumbotron">
+      <h1 class="display-4">Digiruokalista + React</h1>
+      <p class="lead">Ravintoloiden ruokalistat diginä.</p>
+    </div>
       <section>
         <h2 id="titteli">Ravintolat</h2>
         <div id="ravintolalista">
-          <table>
+          <table className='table table-dark'>
             <thead>
               <tr>
                 <th>Nimi</th>
@@ -26,13 +41,23 @@ function Ravintolalista() {
             </thead>
             <tbody>
               {ravintolat.map((rafla) =>
-                <tr key={rafla.id}>
+                <tr key={rafla.id} onClick={() => nakkivene(rafla)}>
                   <td>{rafla.nimi}</td>
                   <td>{rafla.kaupunki}</td> 
                 </tr>
               )}
             </tbody>
           </table>
+        </div>
+        <div id="ravintola">
+          <h3 className='btn btn-primary' onClick={() => Palaa()}>Palaa listaan</h3>
+          {ravintola.map((kat)=>
+          <Card bg="dark" text="light" key={kat.id}>
+            <Card.Body>
+            <Card.Title>{kat.nimi}</Card.Title>
+            </Card.Body>
+          </Card>
+          )}  
         </div>
       </section>
     </div>
